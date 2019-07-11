465192e12527aa82e26444114b1f594563ee1a33
hjlopez <49752106+hjlopez@users.noreply.github.com>
2019-07-11 19:20:36 +0800

    Base Project

---
+++ MyShopDemo.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyShopDemo.WebUI.MvcApplication" Language="C#" %>
---
+++ MyShopDemo.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShopDemo.WebUI.Startup))]
+namespace MyShopDemo.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
